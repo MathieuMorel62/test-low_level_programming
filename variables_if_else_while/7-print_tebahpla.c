07b473877363f7dabc1e8595336de4f82f67b90d
Mathieu Morel <mathieu.morel@holbertonschool.com>
2025-06-22 19:02:15 +0200

    Added file 7-print_tebahpla.c containing a main function that displays the alphabet in reverse order, from ‘z’ to ‘a’, using putchar.

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int rev_alphabet;
+
+	for (rev_alphabet = 122; rev_alphabet >= 97; rev_alphabet--)
+		putchar(rev_alphabet);
+	putchar('\n');
+	
+	return (0);
+}
