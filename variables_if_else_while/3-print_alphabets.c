f15ccdc64c578ada0d93ef44e5e52cce73fdb412
Mathieu Morel <mathieu.morel@holbertonschool.com>
2025-06-22 18:43:56 +0200

    Added comments to 3-print_alphabets.c to clarify the main function's entry point and return value. Improved code readability by adding spaces around for loops.

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char lower;
 	char upper;
 
-	for(lower = 97;  lower < 123; lower++)
+	for (lower = 97;  lower < 123; lower++)
 		putchar(lower);
-	for(upper = 65; upper < 91; upper++)
+	for (upper = 65; upper < 91; upper++)
 		putchar(upper);
 	putchar('\n');
 
