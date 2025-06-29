b26d566cf806ac7318ef39267bb297342f1102eb
Mathieu Morel <mathieu.morel@holbertonschool.com>
2025-06-29 16:15:08 +0200

    Fixed the loop in the print_alphabet_x10 function to display the alphabet ten times instead of eleven.

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	char letters;
 	int count;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count <= 9; count++)
 	{
 		for (letters = 'a'; letters <= 'z'; letters++)
 		{
