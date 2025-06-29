e44cf70196428a059d6ba5e3796a48e60bbbae19
Mathieu Morel <mathieu.morel@holbertonschool.com>
2025-06-29 16:13:37 +0200

    Added print_alphabet_x10 function in 2-print_alphabet_x10.c file to display the alphabet ten times.

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char letters;
+	int count;
+
+	for (count = 0; count <= 10; count++)
+	{
+		for (letters = 'a'; letters <= 'z'; letters++)
+		{
+			_putchar(letters);
+		}
+		_putchar('\n');
+	}
+}
